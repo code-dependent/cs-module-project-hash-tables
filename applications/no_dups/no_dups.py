8897fa8a1acc80ab94e635f0adce9da417a0267b
joshua williams <joshua@joshuas-MBP.fios-router.home>
2020-10-30 10:17:37 -0400

    implemented no_dups.py function

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,10 @@
 def no_dups(s):
     # Your code here
-
-
+    mydict = {v:i for i,v in enumerate(s.split())}
+    rtn_string = []
+    for k in mydict:
+        rtn_string.append(k+" ")
+    return "".join(rtn_string)[:-1]
 
 if __name__ == "__main__":
     print(no_dups(""))
